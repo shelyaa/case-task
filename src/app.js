b9a8018b266e173920b6eaf9535745c353caa82c
Aliona <ashelyaa17@gmail.com>
2026-04-11 00:41:50 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ import express from "express";
 import subscriptionRoutes from "./routes/subscriptions.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "*",
+  }),
+);
+
 app.use(express.json());
 
 app.use("/api", subscriptionRoutes);
