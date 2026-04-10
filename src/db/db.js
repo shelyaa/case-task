5d993e909a1ad6e326ebd0b1362ad777b64f0fed
Aliona <ashelyaa17@gmail.com>
2026-04-10 23:45:24 +0300

    fix: ensure SSL configuration is correctly set in database pool

--- src/db/db.js
+++ src/db/db.js
@@ -9,4 +9,5 @@ export const pool = new pg.Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: process.env.DB_SSL === "false" ? false : { rejectUnauthorized: false },
 });
